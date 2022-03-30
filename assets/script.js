a7a82b85dbcb681b4f5123cb076edd96cb880f94
Andrew Midthun <midth002@gmail.com>
2022-03-30 16:17:51 -0500

    Made a for loop and a if else if conditional statement that compares the momment hour to the hours array. When the hourArray index is equal to the current hour then display the present class. Else if the hours array index is greater than the current hour add the class future. I put the past class insid the html for the default css class to be at for the textarea.

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,24 @@
-var hourDiv = $('.hour')
+var textArea = $('.past')
 var currentTime = moment().format("MMM Do, YYYY"); 
 $("#currentDay").text(currentTime)
 
-var currentHour = moment().hour();
-console.log(typeof currentHour)
+var currentHour = moment().hour() - 7;
+
 var hoursArray = ["8", "9", "10", "11", "12", "13", "14", "15", "16"]
 
 function checkHour() {
-    for(i=0; i < hoursArray.length; i++) {
-     if(hoursArray[i] == currentHour) {
-         
-         console.log(hoursArray[i] + " Is the current hour")
-     }
-        
-      
-       
 
+    for (i=0; i < hoursArray.length; i++) {
+        var whatHour = $('#' + hoursArray[i])
+     
+        if(hoursArray[i] == currentHour) {
+           whatHour.addClass('present')
+        } else if (hoursArray[i] > currentHour) {
+            whatHour.addClass('future') 
+        }
     }
+ 
 }
+
 
 checkHour();
