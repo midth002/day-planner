bb6d748ab5a82fa700e081c510edae178fd9ec5a
Andrew Midthun <midth002@gmail.com>
2022-03-30 15:47:16 -0500

    Made major changes to the structure of the HTMl. Added more rows and divs. Added icons to the save buttons on the right for each time block. Added a function checkHour to the script.js file to check what time it is. If it is the current time from the array then console log that hour.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
-alert("Test")
+var hourDiv = $('.hour')
+var currentTime = moment().format("MMM Do, YYYY"); 
+$("#currentDay").text(currentTime)
+
+var currentHour = moment().hour();
+console.log(typeof currentHour)
+var hoursArray = ["8", "9", "10", "11", "12", "13", "14", "15", "16"]
+
+function checkHour() {
+    for(i=0; i < hoursArray.length; i++) {
+     if(hoursArray[i] == currentHour) {
+         
+         console.log(hoursArray[i] + " Is the current hour")
+     }
+        
+      
+       
+
+    }
+}
+
+checkHour();
