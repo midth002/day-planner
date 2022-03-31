cdfef782a4494a4ca79a7e91d87cddc75d2b7907
Andrew Midthun <midth002@gmail.com>
2022-03-30 22:19:28 -0500

    setting and getting local storage and was able to render the page. Only thing right now to figure out is when we refresh the page, saving in a different time it deletes the other local storages.

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,25 @@
 var textArea = $('.past')
 var currentTime = moment().format("MMM Do, YYYY"); 
+
+var timeBlock = $('.time-block')
+var saveBtn = $('.saveBtn')
+var planner = [];
 $("#currentDay").text(currentTime)
 
 var currentHour = moment().hour() - 7;
 
 var hoursArray = ["8", "9", "10", "11", "12", "13", "14", "15", "16"]
 
+function init() {
+    checkHour();
+    getLocalStorage();
+   
+}
+
 function checkHour() {
 
     for (i=0; i < hoursArray.length; i++) {
         var whatHour = $('#' + hoursArray[i])
-     
         if(hoursArray[i] == currentHour) {
            whatHour.addClass('present')
         } else if (hoursArray[i] > currentHour) {
@@ -21,4 +30,35 @@ function checkHour() {
 }
 
 
-checkHour();
+function getLocalStorage() {
+    storedPlanner = JSON.parse(localStorage.getItem("planner"));
+    
+    console.log(storedPlanner);
+
+    for(i=0; i < storedPlanner.length; i++) {
+        var hourObj = storedPlanner[i].hour;
+        if(hourObj) {
+            $('#' + hourObj).text(storedPlanner[i].planner)
+        } 
+    }
+ 
+}
+
+
+
+$("button").click(setLocalStorage) 
+
+
+function setLocalStorage() {
+    
+    var storedHour =  $(this).attr("data-hour");
+    var storedPlanner = $('#' + storedHour);
+   
+    planner.push({
+        hour: storedHour,
+        planner: storedPlanner.val()
+    })
+    localStorage.setItem("planner", JSON.stringify(planner))
+}
+
+init();
