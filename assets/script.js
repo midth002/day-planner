afcc5b8ba95a4d4c5fd0d5562acb8da438cb2797
Andrew Midthun <midth002@gmail.com>
2022-03-31 15:23:22 -0500

    Reformatted the sheets and commented both script.js and index.html

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,72 @@
 var textArea = $('.past')
-var currentTime = moment().format("dddd MMM Do, YYYY"); 
+var currentTime = moment().format("dddd MMM Do, YYYY");
 
 var timeBlock = $('.time-block')
 var saveBtn = $('.saveBtn')
+// array for the stored inputs by user
 var planner = [];
 var storedPlanner;
+
+// Display the current time in the Jumbotron header
 $("#currentDay").text(currentTime)
 
-var currentHour = moment().hour() -3;
+var currentHour = moment().hour();
 
+// Array for the working hours
 var hoursArray = ["8", "9", "10", "11", "12", "13", "14", "15", "16"]
 
 function init() {
     storedPlanner = JSON.parse(localStorage.getItem("planner"));
 
     // This makes it not an empty array planner when we refresh the page.
-    if(storedPlanner !== null) {
+    if (storedPlanner !== null) {
         planner = storedPlanner
     }
-    
+
     checkHour();
-    renderPlanner();  
+    renderPlanner();
 }
 
+// check the time to see what color class should be displayed
 function checkHour() {
 
-    for (i=0; i < hoursArray.length; i++) {
+    for (i = 0; i < hoursArray.length; i++) {
         var whatHour = $('#' + hoursArray[i])
-        if(hoursArray[i] == currentHour) {
-           whatHour.addClass('present')
+        if (hoursArray[i] == currentHour) {
+            whatHour.addClass('present')
         } else if (hoursArray[i] > currentHour) {
-            whatHour.addClass('future') 
+            whatHour.addClass('future')
         }
     }
- 
+
 }
 
-
+// Render the users input back onto the page if they refresh the page
 function renderPlanner() {
-    // 
-    for(i=0; i < planner.length; i++) {
+    for (i = 0; i < planner.length; i++) {
         var hourObj = storedPlanner[i].hour;
-        if(hourObj) {
+        if (hourObj) {
             $('#' + hourObj).text(storedPlanner[i].planner)
-        } 
+        }
     }
- 
+
 }
 
+// Get stored items from local storage
 function storePlanner() {
     storedPlanner = JSON.parse(localStorage.getItem("planner"));
 }
 
 
 
-$("button").click(setLocalStorage) 
+$("button").click(setLocalStorage)
 
-
+// Set the stored data and push it to the planner array
 function setLocalStorage() {
-    
-    var storedHour =  $(this).attr("data-hour");
+
+    var storedHour = $(this).attr("data-hour");
     var storedPlanner = $('#' + storedHour);
-    
+
     planner.push({
         hour: storedHour,
         planner: storedPlanner.val().trim()
@@ -70,4 +75,5 @@ function setLocalStorage() {
     storePlanner();
 }
 
+// reloaded page init function will call the necessary functions back to display the data to the user
 init();
